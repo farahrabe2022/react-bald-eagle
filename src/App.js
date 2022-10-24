49a0d361666aeb9f5a72fbf1db8829320fe462a7
tianahR <43481097+tianahR@users.noreply.github.com>
2022-10-24 10:36:45 -0400

    Add Component TodoList and AddTodoForm

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import React from 'react';
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
-//array of objects to do 
-const todoList = [
-  {
-    id: 1,
-    title: 'Read lessons',
-  },
-  {
-    id: 2,
-    title: 'Do exercises',
-  },
-  {
-    id: 3,
-    title: 'Complete assignments',
-  }
 
-  ];
+
 
 function App() {
      return (
       
       <div>
          <h1> Todo List </h1>
+         <AddTodoForm/>
+         <TodoList/>
 
-       <ul>
-          {todoList.map(function (item) {
-            return <li>{item.title}</li>;
-        })}
-         
-       </ul>
+       
        </div>
        
        
